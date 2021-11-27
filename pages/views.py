b61e978443cff13f1cdeb8312229c5a95ce63a9f
leeskw <leeskw@nate.com>
2021-11-26 18:31:19 -0800

    created pages and configured navigation links

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # path('', 'views.home', name='home'),
 def home(request):
     return render(request, 'pages/home.html')
+
+
+# path('about', views.about, name='about'),
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+# path('services', views.about, name='services'),
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+# path('contact', views.contact name='contact'),
+def contact(request):
+    return render(request, 'pages/contact.html')
