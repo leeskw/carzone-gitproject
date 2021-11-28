1d7be8804f4ff4a64928fad47fd6607f3bb7efc6
leeskw <leeskw@nate.com>
2021-11-27 19:37:23 -0800

    completed Teams Model

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
+from .models import Team
 
 
 # path('', 'views.home', name='home'),
 def home(request):
-    return render(request, 'pages/home.html')
+    teams = Team.objects.all()
+    context = {
+        'teams': teams,
+    }
+    return render(request, 'pages/home.html', context)
 
 
 # path('about', views.about, name='about'),
 def about(request):
-    return render(request, 'pages/about.html')
+    teams = Team.objects.all()
+    context = {
+        'teams': teams,
+    }
+    return render(request, 'pages/about.html', context)
 
 
 # path('services', views.about, name='services'),
